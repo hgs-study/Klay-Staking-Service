880a3ec8c8ec05c8949543753f254dcc81d33436
hgs-study <hgstudy_@naver.com>
2021-04-17 15:53:59 +0900

    Klay Staking Service 초기설정

---
+++ src/test/java/com/klaystakingservice/KlayStakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaystakingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlayStakingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
