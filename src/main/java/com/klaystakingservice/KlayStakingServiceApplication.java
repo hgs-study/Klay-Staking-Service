652fec7c9ffe33a5a9c4431da8b588d60db46fd3
hgs-study <hgstudy_@naver.com>
2021-12-06 21:41:01 +0900

    @EnableCaching 추가

--- src/main/java/com/klaystakingservice/KlayStakingServiceApplication.java
+++ src/main/java/com/klaystakingservice/KlayStakingServiceApplication.java
@@ -4,11 +4,13 @@ package com.klaystakingservice;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableBatchProcessing
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class KlayStakingServiceApplication {
 
     public static void main(String[] args) {
